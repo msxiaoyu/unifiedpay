53ee1a77ac4441a152c8754cb3ae1e15b6b54b0d
xiaoyu <msxiaoyu@126.com>
2018-03-27 00:38:53 +0800

    init

---
+++ src/main/java/com/mysoft/unifiedpay/UnifiedPayApplication.java
@@ -0,0 +1,15 @@
+package com.mysoft.unifiedpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用入口
+ */
+@SpringBootApplication
+public class UnifiedPayApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(UnifiedPayApplication.class,args);
+    }
+}
